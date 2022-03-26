58128e5a6a21c650059f0ca02bcb9754e6b3152f
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-03-27 10:41:13 +1100

    Add image url from robo hash

--- src/card/CustomCard.js
+++ src/card/CustomCard.js
@@ -4,9 +4,9 @@ import { Button, Card } from "react-bootstrap";
 export const CustomCard = () => {
   return (
     <Card style={{ width: "imgurl" }}>
-      <Card.Img variant="top" src="{imgurl}" />
+      <Card.Img variant="top" src="{https://robohash.org/66.249.70.84.png}" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Shibin</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
